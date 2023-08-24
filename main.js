526db85e22e8c56b32381b244b42e03f27f58cf5
Cyanicmoon <jihyohu@gmail.com>
2023-08-24 22:38:43 +0900

    상품 추가

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-function selectAll(){
+let isOpened = false;
+function select_All(){
     const cb = document.querySelectorAll('input[type="checkbox"]');
     for (let i=1; i<cb.length; i++){
         cb[i].checked = cb[0].checked;
     }
+}
+function open_Add(){
+    if (isOpened) return;
+    
+    isOpened = true;
+    
 }
