ff2dcf286fd5634bf51cefb890daa0587d95b5da
Cyanicmoon <jihyohu@gmail.com>
2023-08-25 08:56:48 +0900

    asdfgh

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const addFrame = `
     <h5>추후 수정이 불가능합니다.</h5>
     <div>
         <p>단가</p>
-        <input type="text" id="price" placeholder="가격을 입력해주세요">
+        <input type="number" id="price" placeholder="가격을 입력해주세요">
     </div>
     <div style="margin-top: 20px;">
         <p>바코드</p>
@@ -22,7 +22,7 @@ const addFrame = `
         <p>분류</p>
         <input type="text" id="type" placeholder="분류를 입력해주세요">
     </div>
-    <button>저장</button>
+    <button onclick="save_Add()">저장</button>
 </div>`
 
 let isOpened = false;
@@ -43,4 +43,32 @@ function close_Add(){
     isOpened = false;
     const container = document.querySelector(".container");
     container.removeChild(container.lastChild);
+}
+function save_Add(){
+    const list = document.querySelector(".item-list");
+    const name = document.querySelector("#name").value;
+    const price = document.querySelector("#price").value;
+    const barcode = document.querySelector("#barcode").value;
+    const type = document.querySelector("#type").value;
+    
+    list.innerHTML += ('<tr>'
+                    +'<td><input type="checkbox"></td>'
+                    +'<td>'+name+'</td>'
+                    +'<td>'+price+'</td>'
+                    +'<td>'+type+'</td>'
+                    +'<td></td>'
+                    +'<td>'+barcode+'</td>'
+                    +'<td></td>'
+                    +'</tr>');
+    close_Add();
+}
+function del_List(){
+    const tr = document.querySelectorAll('tr');
+    for (let i=tr.length-1; i>0; i--){
+        console.log(tr[i].firstElementChild.firstChild.checked);
+        if (tr[i].firstElementChild.firstElementChild.checked = true){
+            tr[i].remove();
+        }
+    }
+
 }
